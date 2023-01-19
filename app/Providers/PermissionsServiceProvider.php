747ef16c52466dd2df06165a74aa61f4698e7f2c
rahulchugh1001 <rahulchugh1001@gmail.com>
2023-01-19 19:02:54 +0530

    website helper done

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -25,43 +25,7 @@ class PermissionsServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-         try {
-            Permission::get()->map(function ($permission) {
-                Gate::define($permission->slug, function ($user) use ($permission) {
-                    return $user->hasPermissionTo($permission);
-                });
-            });
-        } catch (\Exception $e) {
-            report($e);
-            return false;
-        }
-
-        //Blade directives
-        Blade::directive('role', function ($role) {
-             return "<?php if(auth()->check() && auth()->user()->hasRole({$role})) : ?>"; //return this if statement inside php tag
-        });
-
-        Blade::directive('endrole', function ($role) {
-             return "<?php endif; ?>"; //return this endif statement inside php tag
-        });
-
-        Blade::directive('permission', function ($permission) {
-             return "<?php if(auth()->check() && auth()->user()->RoleHasPermission({$permission})) : ?>"; 
-        });
-
-        Blade::directive('endpermission', function ($permission) {
-             return "<?php endif; ?>"; //return this endif statement inside php tag
-        });
-
-        Blade::directive('fieldpermission', function ($permissionsField) {
-             return "<?php if(auth()->check() && auth()->user()->FieldHasPermission({$permissionsField})) : ?>"; 
-        });
-
-        Blade::directive('endfieldpermission', function ($permissionsField) {
-             return "<?php endif; ?>"; //return this endif statement inside php tag
-        });
-
-
-    }
+   {
+     
+   }
 }
