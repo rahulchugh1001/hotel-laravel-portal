d5a933c2e3f36d7c4616a1734319cbc3fef635f2
rahulchugh1001 <rahulchugh1001@gmail.com>
2023-01-19 23:07:01 +0530

    contact mail done

--- app/Http/Controllers/frontend/ContactUsController.php
+++ app/Http/Controllers/frontend/ContactUsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ContactUs;
 use App\Models\WebsiteDetails;
+use Mail;
 
 class ContactUsController extends Controller
 {
@@ -23,22 +24,23 @@ class ContactUsController extends Controller
         $data['message'] = $request['message'];
 
         $save = ContactUs::insert($data);
-        // $this->sendMailToAdmin($data);
+        $this->sendMailToAdmin($data);
         return view('frontend.thankyou');
     }
 
     public function sendMailToAdmin($value){
-    
+
         $data = array('name'=>isset($value['name']) ? $value['name'] : 'Not Mentioned',
                       'email'=>isset($value['email']) ? $value['email'] : 'Not Mentioned',
                       'phone'=>isset($value['phone']) ? $value['phone'] : 'Not Mentioned',
-                      'message'=>isset($value['message']) ? $value['message'] : 'Not Mentioned',
+                      'messageData'=>isset($value['message']) ? $value['message'] : 'Not Mentioned',
                   );
-              Mail::send(['html'=>'admin_mail'], $data, function($message) {
-               $message->to('spregency123@gmail.com', 'SP Residency')->subject
-                  ('One More Lead');
-               $message->from('no-reply@spresidence.com','Admin');
-            });
+        // dd($data['message']);
+           Mail::send(['html'=>'admin_contact_mail'], $data, function($message) {
+         $message->to('spregency123@gmail.com', 'SP Residency')->subject
+            ('One More Lead');
+         $message->from('no-reply@spresidence.com','Admin');
+      });
       
       
           }
